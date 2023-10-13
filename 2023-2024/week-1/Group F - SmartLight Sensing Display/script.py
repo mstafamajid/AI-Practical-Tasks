52541497847cdcb97f50b68f214bdce43d62a150
Mohammed-Azad <mhammadazad2016@gmail.com>
2023-10-13 10:38:03 +0300

    8-puzzle solver initial commit and update smart light

--- 2023-2024/week-1/Group F - SmartLight Sensing Display/script.py
+++ 2023-2024/week-1/Group F - SmartLight Sensing Display/script.py
@@ -1,38 +1,32 @@
-# Title: 2- SmartLight Sensing Display
-# Link: https://github.com/kodo-yousif/AI-Practical-Tasks/tree/main/2023-2024/week-1#2--smartlight-sensing-display
-
 import cv2
 import numpy as np
-
-LowRange=75
-HighRange =145
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise Exception("Error: Could not open camera.")
 
-current_color = np.array([165, 165, 165])
-transition_speed = 0.07
-
 while True:
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #convert RGB to Grayscale
-    brightness = gray.mean() #calculating brightness average depending on one channel Grayscale image
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #convert RGB to Grayscale
 
-    if brightness < LowRange:
-        target_color = np.array([255, 255, 255])
-    elif brightness > HighRange:
-        target_color = np.array([0, 0, 0])
-    else:
-        target_color = np.array([150, 150, 150])
-        
-    # Smoothly transition the current color to the target color
-    current_color = current_color + transition_speed * (target_color - current_color)
+    total_brightness = np.sum(gray_frame)
+    num_pixels = gray_frame.size
+    mean_brightness = total_brightness / num_pixels
+    
+    normalized_brightness = mean_brightness  / 255
+    print("normalized"[normalized_brightness].format(normalized_brightness=normalized_brightness))
 
-    # Display the Frame
-    colored_frame = np.full_like(frame, current_color, dtype=np.uint8)
+    inverted_brightness_percentage = (normalized_brightness ** 2) * 100
 
-    print(brightness)
+    target_color_value = int(inverted_brightness_percentage * 2.55)
+
+    target_color_value = min(255, max(0, target_color_value))
+
+    inverted_target_color_value = 255 - target_color_value
+
+    target_color = np.array([inverted_target_color_value, inverted_target_color_value, inverted_target_color_value])
+    colored_frame = np.full_like(frame, target_color, dtype=np.uint8)
+    
     cv2.imshow("SmartLight Display", colored_frame)
 
     key= cv2.waitKey(1)
