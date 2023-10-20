589cf04b8c1d7418278e3bf01e517512523257ae
sako-ranj <sakoranj2001@gmail.com>
2023-10-20 18:40:37 +0300

    change to dp, add styling to web interface

--- 2023-2024/week-3/Group D - Ordering Books on a Shelf/script.py
+++ 2023-2024/week-3/Group D - Ordering Books on a Shelf/script.py
@@ -1,27 +1,41 @@
 from flask import Flask, request, render_template
-import math
 
+import os
+
+# Specify the directory and file name
+directory = "2023-2024/week-3/Group D - Ordering Books on a Shelf"
+filename = "solution.txt"
+
+# Create the absolute path
+absolute_directory = os.path.abspath(directory)
+absolute_path = os.path.join(absolute_directory, filename)
 
 app = Flask(__name__)
 
+dp = None
+
 
 def permutation(n, r):
+    global dp
     if n >= r:
-        return math.factorial(n) / math.factorial(n - r)
+        dp = [[0] * (r + 1) for _ in range(n + 1)]
+
+        for i in range(n + 1):
+            for j in range(min(i, r) + 1):
+                if j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = dp[i - 1][j] + j * dp[i - 1][j - 1]
+        return dp[n][r]
     else:
         return 0
 
 
-import math
-
-
 def generate_permutation_table(max_n, max_r):
-    with open(
-        "2023-2024\week-3\Group D - Ordering Books on a Shelf\solution.txt", "w"
-    ) as file:
+    with open(absolute_path, "w") as file:
         for n in range(1, max_n + 1):
             for r in range(1, min(n, max_r) + 1):
-                result = permutation(n, r)
+                result = dp[n][r]
                 file.write(f"{n},{r},{result}\n")
 
 
@@ -39,9 +53,7 @@ def index():
 @app.route("/permutation_table")
 def permutation_table():
     table_data = []
-    with open(
-        "2023-2024\week-3\Group D - Ordering Books on a Shelf\solution.txt", "r"
-    ) as file:
+    with open(absolute_path, "r") as file:
         for line in file:
             n, r, result = line.strip().split(",")
             table_data.append((n, r, result))
