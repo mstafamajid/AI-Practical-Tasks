d6d6ecfa8139af31e16be3d69e641689911da2eb
Kodo Yousif <kodo.yousif@gmail.com>
2023-10-25 18:42:43 +0300

    done

--- 2023-2024/week-3/A-start-template/app.py
+++ 2023-2024/week-3/A-start-template/app.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+solutionEdgeId = 1
+def getId():
+    global solutionEdgeId
+    newId = solutionEdgeId
+    solutionEdgeId += 1
+    return newId
 
 @app.route("/", methods=["GET"])
 def index():
@@ -61,7 +67,55 @@ def index():
         "d-f":6,
         "f-g":3
     }
-    return render_template("index.html", nodes=nodes, edges=edges)
+
+ 
+
+
+# label style => step number +  node name + cost spent until now + remaining cost + total cost
+    solutionEdges = {
+        "id": getId(),
+        "label":"1. S 0-17-17",
+        "children":[
+            {
+                "id": getId(),
+                "label":"2. A 6-10-16",
+                "children":[
+                    {
+                        "id": getId(),
+                        "label":"4. A 12-4-16",
+                    }
+                ]
+            },
+            {
+                "id": getId(),
+                "label":"2. C 10-4-14",
+                "children":[
+                    {
+                        "id": getId(),
+                        "label":"3. D 16-2-18",
+                    }
+                ]
+            },
+            {
+                "id": getId(),
+                "label":"2. B 5-13-18",
+                "children":[
+                    {
+                        "id": getId(),
+                        "label":"7. E 10-4-15",
+                    },
+                    {
+                        "id": getId(),
+                        "label":"7. D 12-2-14",
+                    }
+                ]
+            }
+        ]
+    }
+
+    bestPath = ["S 0-17-17", "A 10-2-14", "B 12-5-17", "C 13-3-16", "G 18-1-18"]
+
+    return render_template("index.html", nodes=nodes, edges=edges, solutionEdges=solutionEdges, bestPath=bestPath)
 
 
 if __name__ == "__main__":
