741270e19745c95dc08f467b711b3eb550b8fe0a
back-xy <bawarmohamad100@gmail.com>
2023-10-28 23:45:26 +0300

    a-star refactor

--- 2023-2024/week-3/Group E -/a-star.py
+++ 2023-2024/week-3/Group E -/a-star.py
@@ -12,27 +12,19 @@ class Graph:
         return self.heuristics[n]
 
     def a_star_algorithm(self, start_node, stop_node):
-        # open_list is a list of nodes which have been visited, but who's neighbors
-        # haven't all been inspected, starts off with the start node
-        # closed_list is a list of nodes which have been visited
-        # and who's neighbors have been inspected
         open_list = set([start_node])
         closed_list = set([])
 
-        # g contains current distances from start_node to all other nodes
-        # the default value (if it's not found in the map) is +infinity
         g = {}
 
         g[start_node] = 0
 
-        # parents contains an adjacency map of all nodes
         parents = {}
         parents[start_node] = start_node
 
         while len(open_list) > 0:
             n = None
 
-            # find a node with the lowest value of f() - evaluation function
             for v in open_list:
                 if n == None or g[v] + self.h(v) < g[n] + self.h(n):
                     n = v;
@@ -41,8 +33,6 @@ class Graph:
                 print('Path does not exist!')
                 return None
 
-            # if the current node is the stop_node
-            # then we begin reconstructin the path from it to the start_node
             if n == stop_node:
                 reconst_path = []
 
@@ -57,18 +47,12 @@ class Graph:
                 print('Path found: {}'.format(reconst_path))
                 return reconst_path
 
-            # for all neighbors of the current node do
             for (m, weight) in self.get_neighbors(n):
-                # if the current node isn't in both open_list and closed_list
-                # add it to open_list and note n as it's parent
                 if m not in open_list and m not in closed_list:
                     open_list.add(m)
                     parents[m] = n
                     g[m] = g[n] + weight
 
-                # otherwise, check if it's quicker to first visit n, then m
-                # and if it is, update parent data and g data
-                # and if the node was in the closed_list, move it to open_list
                 else:
                     if g[m] > g[n] + weight:
                         g[m] = g[n] + weight
@@ -78,15 +62,12 @@ class Graph:
                             closed_list.remove(m)
                             open_list.add(m)
 
-            # remove n from the open_list, and add it to closed_list
-            # because all of his neighbors were inspected
             open_list.remove(n)
             closed_list.add(n)
 
         print('Path does not exist!')
         return None
     
-# Define a more complex adjacency list and heuristics
 adjacency_list = {
     'A': [('B', 3), ('C', 6), ('D', 10)],
     'B': [('E', 4), ('F', 8)],
@@ -108,11 +89,9 @@ heuristics = {
     'F': 5,
     'G': 4,
     'H': 3,
-    'I': 0  # Setting the heuristic for the goal node to 0
+    'I': 0
 }
 
-# Create the Graph instance with adjacency list and heuristics
 graph1 = Graph(adjacency_list, heuristics)
 
-# Call the A* algorithm
 graph1.a_star_algorithm('A', 'I')
