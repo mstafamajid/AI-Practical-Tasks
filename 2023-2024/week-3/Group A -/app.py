f775c813c6965fa702d0e57b51427b4d9eccdb5f
mstafamajid <mstafa.00857387@gmail.com>
2023-10-28 22:44:06 +0300

    completeee

--- 2023-2024/week-3/Group A -/app.py
+++ 2023-2024/week-3/Group A -/app.py
@@ -1,23 +1,14 @@
 import json
 from queue import PriorityQueue
-import time
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-solutionEdgeId = 1
-
-
-def getId():
-    global solutionEdgeId
-    newId = solutionEdgeId
-    solutionEdgeId += 1
-    return newId
 
 
 def manhatn_destance(node1, node2):
-# find distance between two nodes by manhatn distance by this rule abs(x1-x2)+abs(y1-y2)
+
     return abs(node1['row']-node2['row'])+abs(node1['col']-node2['col'])
 
 # Define the A* algorithm function
@@ -29,50 +20,83 @@ def astar_search(start, goal, nodes, edges):
     g_score = {node: float('inf') for node in nodes}
     g_score[start] = 0
 
+    # Initialize the solution tree with the start node
+    solution_tree = {
+            "label": f"{start}-{g_score[start]}-{nodes[start]['cost']}-{g_score[start] + nodes[start]['cost']}",
+            "children": []
+        
+    }
 
     while not open_set.empty():
         _, current = open_set.get()
+        
+       
         if current == goal:
-            path = []
+            path = [] #[s,b,e,f,g]
             while current in came_from:
                 path.insert(0, current)
                 current = came_from[current]
-            return path # Return both the path and the solution tree
+            return path, solution_tree  # Return both the path and the solution tree
 
         for neighbor in nodes:
             if neighbor == 's':
                 continue
-#create keys like this s-a for getting costs from edges dic
-            key = "-".join([current, neighbor])
+
+            key = "-".join([current, neighbor]) 
             tentative_g_score = g_score[current] + edges.get((key), float('inf'))
+           
+
             if tentative_g_score < g_score[neighbor]:
-                came_from[neighbor] = current
+                came_from[neighbor] = current  
                 g_score[neighbor] = tentative_g_score
                 f_score = g_score[neighbor] + manhatn_destance(nodes[neighbor], nodes[goal])
 
                 open_set.put((f_score, neighbor))
-
-
                 
-
+               
                 
-         
+                
+            if g_score[neighbor]!=float('inf') and tentative_g_score != float('inf'):
 
-    return None# Return None if no path is found
+                 
+                 neighbor_node = {
+                    "id": 0,
+                    "label": f"{neighbor}-{g_score[neighbor]}-{nodes[neighbor]['cost']}-{g_score[neighbor] + manhatn_destance(nodes[neighbor], nodes[goal])}",
+                    "children": []
+                }
+                 targe=f"{current}-{g_score[current]}-{nodes[current]['cost']}-{g_score[current] + manhatn_destance(nodes[current], nodes[goal])}"
+                 parent_node = find_node(solution_tree, targe)
+                
+                 parent_node["children"].append(neighbor_node)
 
 
+    return None, None  # Return None if no path is found
+def find_node(node, target_label):
+    print(target_label)
+    print("hello")
+    print(node["label"])
+    if target_label == node["label"]:
+            return node
+    for child in node.get("children", []):
+            found_node = find_node(child, target_label)
+            if found_node:
+                return found_node
+    return None
+# Example usage:
 
 
 
 @app.route("/", methods=["GET"])
 def index():
-# nodes
+
     nodes = {
+        
         "s": {
             "row": 0,
             "col": 2,
             "cost": 99
         },
+        
         "a": {
             "row": 2,
             "col": 0,
@@ -110,8 +134,9 @@ def index():
         },
     }
 
-   
-# real costs
+    for node1 in nodes:
+        nodes[node1]["cost"] = manhatn_destance(nodes[node1], nodes["g"])
+
     edges = {
         "s-a": 6,
         "s-b": 5,
@@ -125,68 +150,19 @@ def index():
         "f-g": 3
     }
 
-
-
-    solutionEdges = {
-        "id": getId(),
-        "label": "1. S 0-17-17",
-        "children": [
-            {
-                "id": getId(),
-                "label": "2. A 6-10-16",
-                "children": [
-                    {
-                        "id": getId(),
-                        "label": "4. A 12-4-16",
-                    }
-                ]
-            },
-            {
-                "id": getId(),
-                "label": "2. C 10-4-14",
-                "children": [
-                    {
-                        "id": getId(),
-                        "label": "3. D 16-2-18",
-                    }
-                ]
-            },
-            {
-                "id": getId(),
-                "label": "2. B 5-13-18",
-                "children": [
-                    {
-                        "id": getId(),
-                        "label": "7. E 10-4-15",
-                    },
-                    {
-                        "id": getId(),
-                        "label": "7. D 12-2-14",
-                    }
-                ]
-            }
-        ]
-    }
-    
     # Use the A* algorithm to find the best path
     start_node = "s"
     goal_node = "g"
-    # set heurisitc distance for each node by manhatn distance
-    for node1 in nodes:
-        nodes[node1]["cost"] = manhatn_destance(nodes[node1], nodes[goal_node])
-    best_path_nodes = astar_search(start_node, goal_node, nodes,edges)
+    best_path_nodes,solution = astar_search(start_node, goal_node, nodes,edges)
     
-  
+ 
 
-    
-    
     if best_path_nodes:
         best_path = []
         spent_cost=0
-        # add start node to the begin of path
         best_path_nodes.insert(0,start_node)
 
-# for creating labels on best path solution cards
+
     for i, node in enumerate(best_path_nodes):
             if i!=0:
                 cost = edges.get("-".join([best_path_nodes[i-1],best_path_nodes[i]]))
@@ -199,8 +175,8 @@ def index():
             
             step_label = f"{node} {spent_cost}-{remaining_cost}-{total_cost}"
             best_path.append(step_label)
-
-    return render_template("index.html", nodes=nodes, edges=edges, bestPath=best_path)
+            
+    return render_template("index.html", nodes=nodes, edges=edges, solutionEdges=solution, bestPath=best_path)
 
 if __name__ == "__main__":
     app.run(debug=True)
