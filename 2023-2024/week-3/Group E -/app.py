875c0bb113196024f2e6402370e587c1910675b7
back-xy <bawarmohamad100@gmail.com>
2023-10-30 21:33:03 +0300

    week-3 group-e final solution

--- 2023-2024/week-3/Group E -/app.py
+++ 2023-2024/week-3/Group E -/app.py
@@ -13,20 +13,59 @@ def getId():
 @app.route("/", methods=["GET"])
 def index():
     nodes = {
-        "s": {"row": 0, "col": 1, "cost": 99},
-        "a": {"row": 2, "col": 0, "cost": 99},
-        "b": {"row": 2, "col": 2, "cost": 99},
-        "c": {"row": 4, "col": 1, "cost": 99},
-        "g": {"row": 4, "col": 3, "cost": 99},
+        "s":{
+            "row":0,
+            "col":2,
+            "cost":99
+        },
+        "a":{
+            "row":2,
+            "col":0,
+            "cost":99
+        },
+        "b":{
+            "row":2,
+            "col":1,
+            "cost":99
+        },
+        "c":{
+            "row":2,
+            "col":3,
+            "cost":99
+        },
+        "e":{
+            "row":4,
+            "col":1,
+            "cost":99
+        },
+        "d":{
+            "row":4,
+            "col":2,
+            "cost":99
+        },
+        "f":{
+            "row":5,
+            "col":2,
+            "cost":99
+        },
+        "g":{
+            "row":6,
+            "col":0,
+            "cost":99
+        },
     }
 
-    # g(n)
     edges = {
         "s-a":6,
-        "s-b":6,
-        "a-c":4,
-        "b-c":4,
-        "c-g":2
+        "s-b":5,
+        "s-c":10,
+        "a-e":6,
+        "b-e":6,
+        "b-d":7,
+        "c-d":6,
+        "e-f":4,
+        "d-f":6,
+        "f-g":3
     }
 
     # ============================
