755d07ca9967e2251e305bc0f8072c1915f749ff
back-xy <bawarmohamad100@gmail.com>
2023-12-13 04:50:11 +0300

    adding more comments

--- 2023-2024/week-5/Group E - SVM Model/app.py
+++ 2023-2024/week-5/Group E - SVM Model/app.py
@@ -5,11 +5,11 @@ from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.metrics import accuracy_score
 
 # Import the Iris dataset from scikit-learn
-iris = datasets.load_iris()
+iris = datasets.load_iris() # 2D Array (table)
 
 # Extract the first two features for simplicity
-X = iris.data[:, :2]
-y = iris.target
+X = iris.data[:, :2] # length, Width
+y = iris.target # Setosa, Versicolor, Virginica
 
 # Create three SVM models with different kernels: linear, RBF, and polynomial
 models = (
@@ -18,7 +18,8 @@ models = (
     svm.SVC(kernel="poly", gamma=1.0),
 )
 
-# Fit each model to the data
+# Fit each model to the data (train the models)
+# clf.fit(X, y) is the training method
 models = (clf.fit(X, y) for clf in models)
 
 # Titles for the subplots
@@ -32,38 +33,37 @@ titles = (
 fig, sub = plt.subplots(2, 2)
 plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
-# Extract the first and second features for clarity in plotting
+# Extract the first and second feature datas for clarity in plotting
+# (y,x) Widths, Lengths
 X0, X1 = X[:, 0], X[:, 1]
 
 # Loop through each model, title, and subplot
-for clf, title, ax in zip(models, titles, sub.flatten()):
-    # Create a decision boundary display for the current model
-    disp = DecisionBoundaryDisplay.from_estimator(
+for clf, title, subplot in zip(models, titles, sub.flatten()):
+    # Create a decision boundary display for the current model (classify)
+    DecisionBoundaryDisplay.from_estimator(
         clf,
         X,
-        response_method="predict",
-        cmap=plt.cm.coolwarm,
-        alpha=0.8,
-        ax=ax,
+        cmap=plt.cm.coolwarm, # color map
+        alpha=0.8, # transparency of the regions
+        ax=subplot,
         xlabel=iris.feature_names[0],
         ylabel=iris.feature_names[1],
     )
-    
+
     # Get model predictions
     y_pred = clf.predict(X)
-
     # Compute accuracy
     accuracy = accuracy_score(y, y_pred)
     
     # Scatter plot the data points with colors based on the true labels
-    ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors="k")
+    subplot.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors="k")
     
     # Remove x and y ticks for clarity
-    ax.set_xticks(())
-    ax.set_yticks(())
+    subplot.set_xticks(())
+    subplot.set_yticks(())
     
     # Include accuracy in the title of each subplot
-    ax.set_title(f"{title}\nAccuracy: {accuracy:.2f}")
+    subplot.set_title(f"{title}\nAccuracy: {accuracy:.2f}")
 
 # Show the plot
 plt.show()
