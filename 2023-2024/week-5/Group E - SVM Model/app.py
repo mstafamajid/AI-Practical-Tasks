cb0f435ba3e122dadcde4d74d57708cc6c7c4540
back-xy <bawarmohamad100@gmail.com>
2023-12-13 04:08:12 +0300

    writing comments to clarify code

--- 2023-2024/week-5/Group E - SVM Model/app.py
+++ 2023-2024/week-5/Group E - SVM Model/app.py
@@ -1,37 +1,43 @@
+# Import necessary libraries
 import matplotlib.pyplot as plt
 from sklearn import datasets, svm
 from sklearn.inspection import DecisionBoundaryDisplay
 from sklearn.metrics import accuracy_score
 
-# import some data to play with
+# Import the Iris dataset from scikit-learn
 iris = datasets.load_iris()
-# Take the first two features. We could avoid this by using a two-dim dataset
+
+# Extract the first two features for simplicity
 X = iris.data[:, :2]
 y = iris.target
 
-# we create an instance of SVM and fit out data. We do not scale our
-# data since we want to plot the support vectors
+# Create three SVM models with different kernels: linear, RBF, and polynomial
 models = (
     svm.SVC(kernel="linear", gamma=1.0),
     svm.SVC(kernel="rbf", gamma=1.0),
     svm.SVC(kernel="poly", gamma=1.0),
 )
+
+# Fit each model to the data
 models = (clf.fit(X, y) for clf in models)
 
-# title for the plots
+# Titles for the subplots
 titles = (
     "SVC with linear kernel",
     "SVC with RBF kernel",
     "SVC with polynomial kernel",
 )
 
-# Set-up 2x2 grid for plotting.
+# Set up a 2x2 grid for subplots
 fig, sub = plt.subplots(2, 2)
 plt.subplots_adjust(wspace=0.4, hspace=0.4)
 
+# Extract the first and second features for clarity in plotting
 X0, X1 = X[:, 0], X[:, 1]
 
+# Loop through each model, title, and subplot
 for clf, title, ax in zip(models, titles, sub.flatten()):
+    # Create a decision boundary display for the current model
     disp = DecisionBoundaryDisplay.from_estimator(
         clf,
         X,
@@ -49,12 +55,15 @@ for clf, title, ax in zip(models, titles, sub.flatten()):
     # Compute accuracy
     accuracy = accuracy_score(y, y_pred)
     
+    # Scatter plot the data points with colors based on the true labels
     ax.scatter(X0, X1, c=y, cmap=plt.cm.coolwarm, s=20, edgecolors="k")
     
+    # Remove x and y ticks for clarity
     ax.set_xticks(())
     ax.set_yticks(())
     
-    # Include accuracy in the title
+    # Include accuracy in the title of each subplot
     ax.set_title(f"{title}\nAccuracy: {accuracy:.2f}")
 
+# Show the plot
 plt.show()
